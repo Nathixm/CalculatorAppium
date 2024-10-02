1b49ada1fdc6b2accce5a86b2babd059b64d764a
Nathália Martins <nath.html@gmail.com>
2024-10-01 23:13:16 -0300

    Projeto final

--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -1,8 +1,8 @@
 describe('Testando Calculadora Google', () => {
     before(async () => {
-        const começo = ''
-   
-        await $(começo).waitForDisplayed();
+        const calculadora = '//android.view.ViewGroup[@resource-id="com.google.android.calculator:id/main_calculator"]'
+        await $(calculadora).waitForDisplayed();
+
     });
  
     it('Soma', async () => {
@@ -10,28 +10,94 @@ describe('Testando Calculadora Google', () => {
         const numero1 = '//android.widget.ImageButton[@content-desc="5"]';
         await $(numero1).click()
 
+        const numero2 = '//android.widget.ImageButton[@content-desc="4"]';
+        await $(numero2).click()
+
         const soma = 'accessibility id:plus';
         await $(soma).click()
 
-        const numero2 = '//android.widget.ImageButton[@content-desc="9"]';
-        await $(numero2).click()
+        const numero3 = '//android.widget.ImageButton[@content-desc="9"]';
+        await $(numero3).click()
+
+        const numero4 = '//android.widget.ImageButton[@content-desc="5"]';
+        await $(numero4).click()
 
         const equals = 'accessibility id:equals';
         await $(equals).click();
 
         const resultado = 'id:com.google.android.calculator:id/result_final';
-        expect(await $(resultado).getText()).toEqual('14');
+        expect(await $(resultado).getText()).toEqual('149');
+
+    });
+
+    it('Subtração', async () => {
+
+        const numero1 = '//android.widget.ImageButton[@content-desc="8"]';
+        await $(numero1).click()
+
+        const numero2 = '//android.widget.ImageButton[@content-desc="4"]';
+        await $(numero2).click()
+
+        const subtração = 'accessibility id:minus';
+        await $(subtração).click()
+
+        const numero3 = '//android.widget.ImageButton[@content-desc="3"]';
+        await $(numero3).click()
+
+        const numero4 = '//android.widget.ImageButton[@content-desc="1"]';
+        await $(numero4).click()
+
+        const equals = 'accessibility id:equals';
+        await $(equals).click();
+
+        const resultado = 'id:com.google.android.calculator:id/result_final';
+        expect(await $(resultado).getText()).toEqual('53');
+
+    });
+
+    it('Multiplicação', async () => {
+
+        const numero1 = '//android.widget.ImageButton[@content-desc="3"]';
+        await $(numero1).click()
+
+        const numero2 = '//android.widget.ImageButton[@content-desc="2"]';
+        await $(numero2).click()
+
+        const multiplicacao = 'accessibility id:multiply';
+        await $(multiplicacao).click()
+
+        const numero3 = '//android.widget.ImageButton[@content-desc="5"]';
+        await $(numero3).click()
+
+        const equals = 'accessibility id:equals';
+        await $(equals).click();
+
+        const resultado = 'id:com.google.android.calculator:id/result_final';
+        expect(await $(resultado).getText()).toEqual('160');
+
+    });
+
+    it('Divisão', async () => {
+
+        const numero1 = '//android.widget.ImageButton[@content-desc="4"]';
+        await $(numero1).click()
+
+        const numero2 = '//android.widget.ImageButton[@content-desc="0"]';
+        await $(numero2).click()
+
+        const divisao= 'accessibility id:divide';
+        await $(divisao).click()
+
+        const numero3 = '//android.widget.ImageButton[@content-desc="2"]';
+        await $(numero3).click()
+
+        const equals = 'accessibility id:equals';
+        await $(equals).click();
+
+        const resultado = 'id:com.google.android.calculator:id/result_final';
+        expect(await $(resultado).getText()).toEqual('20');
 
     });
 
 
 });
-
-
-/*const el5 = await driver.$("accessibility id:minus");
-await el5.click();
-const el6 = await driver.$("accessibility id:multiply");
-await el6.click();
-const el7 = await driver.$("accessibility id:divide");
-await el7.click();
-const el9 = await driver.$("accessibility id:equals"); */
